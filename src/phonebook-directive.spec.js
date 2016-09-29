057e77913efd6e83e88114992ecf0e83c4ca8c95
Joel Jeske <jjeske@netspend.com>
2016-09-29 12:01:26 -0500

    Flesh out

--- src/phonebook-directive.spec.js
+++ src/phonebook-directive.spec.js
@@ -1,1 +1,37 @@
 
+directiveTest({
+  module: angular.module('app'),
+  name: 'phonebookDirective',
+  template: '<phonebook phone-numbers="testNumbers"></phonebook>',
+  controller: 'phonebook',
+  // Selector based assertions. Key (CSS selector) should contain Value (Number) of matches
+  selectors: {
+    'phonenumber': 0
+  },
+  // Custom assertions. Functions are injectable
+  assertions: {
+    'create phone numbers for each entry': function($element, $scope) {
+      $scope.testNumbers = ['0', '1', '2'];
+      $scope.$apply();
+      expect($element.find('phonenumber').length).toBe(3);
+    },
+    'expose refresh numbers on scope': function($element) {
+      var isolate = $element.isolateScope();
+      expect(isolate.refresh).toBeDefined();
+    },
+    'should delegate to phone model': function($element, $scope, phoneModel, $q) {
+      var isolate = $element.isolateScope();
+      var testNumbers = ['0', '1', '2'];
+      spyOn(phoneModel, 'get').and.callFake(function() {
+        return $q(function(resolve) {
+          resolve(testNumbers);
+        });
+      });
+
+      isolate.refresh();
+      $scope.$apply();
+
+      expect($scope.testNumbers).toEqual(testNumbers);
+    }
+  }
+});
