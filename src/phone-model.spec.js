31fc47486b19b8ae07a4e67bad84b0060d44aa5c
Joel Jeske <jjeske@netspend.com>
2016-09-29 11:48:04 -0500

    Async model test skeleton

---
+++ src/phone-model.spec.js
@@ -0,0 +1,24 @@
+describe('Factory: phoneModel', function() {
+
+  beforeEach(module('app'));
+  beforeEach(inject(function(phoneModel, $httpBackend) {
+
+  }));
+
+  afterEach(function() {
+
+  });
+
+  it('should exist', function() {
+
+  });
+
+  it('should request on get()', function() {
+
+  });
+
+  it('should post on update()', function() {
+
+  });
+
+});
